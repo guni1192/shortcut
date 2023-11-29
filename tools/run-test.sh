8b5170351ead9dcf8a1353fcd0ea0f0b6cd9a7cd
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-11-29 23:44:00 +0900

    [CI] Run db migration without docker
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- tools/run-test.sh
+++ tools/run-test.sh
@@ -2,6 +2,12 @@
 
 set -eux
 
+source .env
+
 docker compose build
 docker compose up -d
+
+cargo install sqlx-cli --no-default-features --features rustls,mysql
+cargo sqlx migrate run
+
 cargo test --tests
