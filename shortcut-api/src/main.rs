0f4f10cd346a477cb138af4dc5725f89ab0c029d
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-12-09 10:34:15 +0900

    setup tracing and tracing subscriber
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- shortcut-api/src/main.rs
+++ shortcut-api/src/main.rs
@@ -1,11 +1,17 @@
 use shortcut_api::services::shortcut::{proto::shortcut_server::ShortcutServer, ShortcutService};
 use tonic::transport::Server;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let addr = "0.0.0.0:1192".parse().unwrap();
     let shortcut = ShortcutService::default();
 
+    info!("Starting server at {}", addr);
     Server::builder()
         .add_service(ShortcutServer::new(shortcut))
         .serve(addr)
